a6460ed0852541b2455e8e0fc960a48fd1bac6f7
Mauricio Linhares <mauricio.linhares@gmail.com>
2016-05-16 01:22:28 -0400

    Introducing rwmutex

--- store/memory.go
+++ store/memory.go
@@ -1,5 +1,7 @@
 package store
 
+import "sync"
+
 type Storage interface {
 	Put(key string, value []byte)
 	Get(key string) ([]byte, bool)
@@ -10,6 +12,7 @@ type keyPairs map[string][]byte
 
 type memoryStorage struct {
 	items keyPairs
+	lock  sync.RWMutex
 }
 
 func New() Storage {
@@ -19,14 +22,20 @@ func New() Storage {
 }
 
 func (m *memoryStorage) Put(key string, value []byte) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
 	m.items[key] = value
 }
 
 func (m *memoryStorage) Get(key string) ([]byte, bool) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 	value, ok := m.items[key]
 	return value, ok
 }
 
 func (m *memoryStorage) Delete(key string) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
 	delete(m.items, key)
 }
